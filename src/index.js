31be9829f2db82993fa3883608c10e8578606692
Taylor <tbaustin1992@gmail.com>
2019-02-25 10:40:14 -0600

    update prop

--- src/index.js
+++ src/index.js
@@ -2,25 +2,28 @@ import React from 'react'
 
 export default class Dotmailer extends React.Component {
 	render() {
-		const { src, id, className = "ds-if" } = this.props
+		const { src, id, className = 'ds-if' } = this.props
 		const embed = src.indexOf(`/embed/script/`) !== -1
 
 		return (
-			<iframe 
+			<iframe
 				className={className}
-				id={id} 
-				name={id} 
-				src={embed ? `//r2.dotmailer-surveys.com/Response/Survey/${id}?source=e&name=${id}` : src} 
+				id={id}
+				name={id}
+				src={
+					embed
+						? `//r2.dotmailer-surveys.com/Response/Survey/${id}?source=e&name=${id}`
+						: src
+				}
 				width="100%"
 				frameBorder="0"
-				scrolling="no"
+				scrolling={`${this.props.scrolling}`}
 				style={{
 					border: `none 0!important`,
 					margin: `0!important`,
-					padding: `0!important`,
+					padding: `0!important`
 				}}
-			>
-			</iframe>
+			/>
 		)
 	}
 }
